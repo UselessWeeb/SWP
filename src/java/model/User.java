887d619a440a7f702732a8f66262fa752d35707a
Loel <sansunderswap2004@gmail.com>
2024-05-15 18:40:02 +0700

    Adding DBContext

---
+++ src/java/model/User.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author vudai
+ */
+public class User {
+    private int idUser;
+    private String fullname;
+    private String email;
+    private String diaChi;
+    private String phoneNum;
+    private int role;
+    private String gender;
+    private int point;
+
+    public User() {
+    }
+
+    public User(int idUser, String fullname, String email, String diaChi, String phoneNum, int role, String gender, int point) {
+        this.idUser = idUser;
+        this.fullname = fullname;
+        this.email = email;
+        this.diaChi = diaChi;
+        this.phoneNum = phoneNum;
+        this.role = role;
+        this.gender = gender;
+        this.point = point;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    
+    
+}
