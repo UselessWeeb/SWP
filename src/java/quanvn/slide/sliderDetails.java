fc921053e25965caa7b20987fd2e33dbdb8ba103
quanvnhe182357 <quanvnhe182357@fpt.edu.vn>
2024-05-20 23:28:32 +0700

    Add Product.java, DBContext.java and orderDetails.java

--- src/java/quanvn/slide/sliderDetails.java
+++ src/java/quanvn/slide/sliderDetails.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package quanvn;
+package quanvn.slide;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
