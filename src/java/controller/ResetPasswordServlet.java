a5d98069934ec8d4138d29cd020b1b33efb29108
Phạm Nguyễn Quốc Anh (K18 HL) <anhpnqhe180829@fpt.edu.vn>
2024-05-22 14:39:48 +0700

    Update ResetPasswordServlet

--- src/java/controller/ResetPasswordServlet.java
+++ src/java/controller/ResetPasswordServlet.java
@@ -2,14 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
+import dao.TokenDAO;
+import dao.UserDAO;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Token;
 
 /**
  *
@@ -18,8 +20,9 @@ import jakarta.servlet.http.HttpServletResponse;
 public class ResetPasswordServlet extends HttpServlet {
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -27,12 +30,33 @@ public class ResetPasswordServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        request.getRequestDispatcher("view/resetPassword.jsp").forward(request, response);
-    } 
+            throws ServletException, IOException {
+        String token = request.getParameter("token");
 
-    /** 
+        if (token == null || token.length() != 32) {
+            response.sendRedirect("index.jsp");
+            return;
+        }
+
+        TokenDAO tokenDao = new TokenDAO();
+        UserDAO userDao = new UserDAO();
+        Token retrievedToken = tokenDao.getToken(token);
+
+        if (retrievedToken != null && retrievedToken.getPurpose() == 1 && !retrievedToken.isExpired()) {
+            request.setAttribute("token", token);
+            request.setAttribute("userID", retrievedToken.getUserID());
+            request.setAttribute("isValid", true);
+            
+            request.getRequestDispatcher("reset-password.jsp").forward(request, response);
+        } else if (retrievedToken != null && retrievedToken.isExpired()) {
+            tokenDao.deleteToken(token);
+            response.sendRedirect("index.jsp");
+        }
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -40,12 +64,13 @@ public class ResetPasswordServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        
+            throws ServletException, IOException {
+
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
